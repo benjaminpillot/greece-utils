b7bd7ba281f80dae855b227b8736a44404ff69ff
pillot <benjamin.pillot@ird.fr>
2019-09-27 11:45:03 +0200

    New version

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,7 @@
 More detailed description.
 """
 
-__version__ = '0.1.2'
+__version__ = '0.1.21'
 __author__ = 'Benjamin Pillot'
 __copyright__ = 'Copyright 2019, Benjamin Pillot'
 __email__ = 'benjaminpillot@riseup.net'
